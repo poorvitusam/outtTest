bb2d143faf1ec19cb99826fd8e483d47e50f0340
poorvitusam <poorvitusam@gmail.com>
2017-04-16 19:38:36 -0400

    responsive fix

--- index.js
+++ index.js
@@ -140,34 +140,34 @@ $(document).ready(function(){
 	  autoplay: true,
 	  autoplaySpeed: 500,
 	  fade: true,
-	  responsive: [
-	    {
-	      breakpoint: 720,
-	      settings: {
-	        slidesToShow: 3,
-	        slidesToScroll: 3,
-	        infinite: true,
-	        dots: true
-	      }
-	    },
-	    {
-	      breakpoint: 520,
-	      settings: {
-	        slidesToShow: 2,
-	        slidesToScroll: 2
-	      }
-	    },
-	    {
-	      breakpoint: 320,
-	      settings: {
-	        slidesToShow: 1,
-	        slidesToScroll: 1
-	      }
-	    }
-	    // You can unslick at a given breakpoint now by adding:
-	    // settings: "unslick"
-	    // instead of a settings object
-	  ]
+	  // responsive: [
+	  //   {
+	  //     breakpoint: 720,
+	  //     settings: {
+	  //       slidesToShow: 3,
+	  //       slidesToScroll: 3,
+	  //       infinite: true,
+	  //       dots: true
+	  //     }
+	  //   },
+	  //   {
+	  //     breakpoint: 520,
+	  //     settings: {
+	  //       slidesToShow: 2,
+	  //       slidesToScroll: 2
+	  //     }
+	  //   },
+	  //   {
+	  //     breakpoint: 320,
+	  //     settings: {
+	  //       slidesToShow: 1,
+	  //       slidesToScroll: 1
+	  //     }
+	  //   }
+	  //   // You can unslick at a given breakpoint now by adding:
+	  //   // settings: "unslick"
+	  //   // instead of a settings object
+	  // ]
 	});
 
 	//Responsive js
